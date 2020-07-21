0609ee73dff8bb6f597cbceaa95a6f6d6888968b
samedsay <say16@itu.edu.tr>
2020-07-21 04:50:12 +0200

    Chapter01_01 is solved.

---
+++ Chapter01/src/main/Solution01_01.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Solution01_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
+		
+	}
+
+}
