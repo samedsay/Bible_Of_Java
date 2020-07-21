fd82881a546e1bbd57a713dc2fc4e29e0edd4885
samedsay <say16@itu.edu.tr>
2020-07-21 05:04:20 +0200

    01_02 solved.

---
+++ Chapter01/src/main/Solution01_02.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Solution01_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+
+	}
+
+}
