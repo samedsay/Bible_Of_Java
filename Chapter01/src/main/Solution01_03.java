5b1cf0cd3662ab59d90b2942d0e14ed8d995dc4f
samedsay <say16@itu.edu.tr>
2020-07-21 05:28:06 +0200

    Chapter01_03 solved.

---
+++ Chapter01/src/main/Solution01_03.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Solution01_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("    J     A     V     V     A");
+		System.out.println("    J    A A     V   V     A A " );
+		System.out.println("J   J   AAAAA     V V     AAAAA");
+		System.out.println(" J J   A     A     V     A     A");
+
+		
+	}
+
+}
