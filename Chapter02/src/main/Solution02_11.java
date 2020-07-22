fbdc27f23f3e3cb7fc622c0810a0b4c2e992d6fb
samedsay <say16@itu.edu.tr>
2020-07-22 11:41:38 +0200

    Solution02_11 solved

---
+++ Chapter02/src/main/Solution02_11.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.util.Scanner;
+
+/*
+ * (Population projection)
+ * Rewrite Programming Exercise 1.11 to prompt the user to enter the number of years and displays the population after the number of years.
+ * Use the hint in Programming Exercise 1.11 for this program.
+ * The population should be cast into an integer. 
+ */
+
+public class Solution02_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int year = input.nextInt();
+		System.out.println((int)(year * (((365 * 24 * 60 * 60) / 7.0) + ((365 * 24 * 60 * 60) / 45.0)
+				- ((365 * 24 * 60 * 60) / 13.0)) + 312_032_486));
+		
+	}
+
+}
