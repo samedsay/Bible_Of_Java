ff783f014266a5c29b001fde44f33cdbb6ea285c
samedsay <say16@itu.edu.tr>
2020-07-23 18:36:34 +0200

    Updated

---
+++ Chapter03/src/main/Solution03_07.java
@@ -0,0 +1,16 @@
+package main;
+
+/*
+ * (Financial application: monetary units)
+ * Modify Listing 2.10, ComputeChange .java, to display the nonzero denominations only,
+ * using singular words for single units such as 1 dollar and 1 penny, and plural words for more than one unit such as 2 dollars and 3 pennies.
+ */
+
+public class Solution03_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
