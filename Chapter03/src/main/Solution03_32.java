74ac795e0601f1bd8c8a20de958b873d6937746e
samedsay <say16@itu.edu.tr>
2020-07-28 23:12:03 +0200

    updated

--- Chapter03/src/main/Solution03_32.java
+++ Chapter03/src/main/Solution03_32.java
@@ -1,5 +1,13 @@
 package main;
 
+/*
+ * Geometry: point position)
+ * Given a directed line from point p0(x0, y0) to p1(x1, y1), you can use the following condition to decide 
+ * whether a point p2(x2, y2) is on the left of the line, on the right, or on the same line
+ * 		(x1 - x0)*(y2 - y0) - (x2 - x0)*(y1 - y0)
+ * 
+ */
+
 import java.util.Scanner;
 
 public class Solution03_32 {
