bd67db2fd8664fb56d06e49c32faf92b73d33a60
samedsay <say16@itu.edu.tr>
2020-08-02 23:22:50 +0200

    updated

--- Chapter04/src/main/Solution04_01.java
+++ Chapter04/src/main/Solution04_01.java
@@ -1,9 +1,19 @@
 package main;
 
+import java.util.Scanner;
+
 public class Solution04_01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Enter a character: ");
+		Scanner input = new Scanner(System.in);
+		String a = input.nextLine();
+		
+		int b = Integer.parseInt(a);
+		char x = (char)b;
+		System.out.println(x);
+	 
 
 	}
 
