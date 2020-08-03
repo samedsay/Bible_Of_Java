a157b2e6246c063d56d4223ea17d12f09d201ed4
samedsay <say16@itu.edu.tr>
2020-08-03 23:29:58 +0200

    updated

--- Chapter04/src/main/Solution04_01.java
+++ Chapter04/src/main/Solution04_01.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 public class Solution04_01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Enter a character: ");
-		Scanner input = new Scanner(System.in);
-		String a = input.nextLine();
+	
+		String a = "Hello";
+		String b = "Hello";
+		String c = a;
+		c = "Hello2";
 		
-		int b = Integer.parseInt(a);
-		char x = (char)b;
-		System.out.println(x);
+		System.out.println(a.equals(c));
 	 
 
 	}
