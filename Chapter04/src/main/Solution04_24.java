6b78563591ae4058db1d7100fa4e117f1502de25
samedsay <say16@itu.edu.tr>
2020-08-18 21:09:17 +0200

    updated

--- Chapter04/src/main/Solution04_24.java
+++ Chapter04/src/main/Solution04_24.java
@@ -1,12 +1,17 @@
 package main;
 
+/*
+ * (Order three cities)
+ * Write a program that prompts the user to enter three cities and displays them in ascending order
+ */
+
 import java.util.Scanner;
 
 public class Solution04_24 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the first city: ");
 		String firstCity = input.next();
@@ -14,31 +19,29 @@ public class Solution04_24 {
 		String secondCity = input.next();
 		System.out.println("Enter the third city: ");
 		String thirdCity = input.next();
-		
+
 		System.out.print("The three cities in alphabetical order are ");
 
-		
-		if(firstCity.compareTo(secondCity) <= 0 && firstCity.compareTo(thirdCity) <= 0) {
-			if(secondCity.compareTo(thirdCity) <= 0)
+		if (firstCity.compareTo(secondCity) <= 0 && firstCity.compareTo(thirdCity) <= 0) {
+			if (secondCity.compareTo(thirdCity) <= 0)
 				System.out.println(firstCity + " " + secondCity + " " + thirdCity);
 			else
 				System.out.println(firstCity + " " + thirdCity + " " + secondCity);
 
-				
-		}else if(secondCity.compareTo(firstCity) <= 0 && secondCity.compareTo(thirdCity) <= 0) {
-			if(firstCity.compareTo(thirdCity) <= 0)
+		} else if (secondCity.compareTo(firstCity) <= 0 && secondCity.compareTo(thirdCity) <= 0) {
+			if (firstCity.compareTo(thirdCity) <= 0)
 				System.out.println(secondCity + " " + firstCity + " " + thirdCity);
 			else
-				System.out.println(secondCity + " " + thirdCity + " " + firstCity);		
-		}else {
-			if(firstCity.compareTo(secondCity) <= 0)
-				System.out.println(thirdCity + " " + firstCity + " " +  secondCity);
+				System.out.println(secondCity + " " + thirdCity + " " + firstCity);
+		} else {
+			if (firstCity.compareTo(secondCity) <= 0)
+				System.out.println(thirdCity + " " + firstCity + " " + secondCity);
 			else
-				System.out.println(thirdCity + " " + secondCity + " " + firstCity);	
+				System.out.println(thirdCity + " " + secondCity + " " + firstCity);
 		}
-		
+
 		input.close();
-		
+
 	}
 
 }
