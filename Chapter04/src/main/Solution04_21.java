26ac32685326747f03dfdfe3a9de4467f776d3cf
samedsay <say16@itu.edu.tr>
2020-08-18 19:57:25 +0200

    updated

--- Chapter04/src/main/Solution04_21.java
+++ Chapter04/src/main/Solution04_21.java
@@ -12,18 +12,20 @@ public class Solution04_21 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter a SSN: ");
 		String number = input.next();
 		System.out.println(number);
-		if(number.charAt(3) != '-' || number.charAt(6) != '-' || number.length() != 11) {
+		if (number.charAt(3) != '-' || number.charAt(6) != '-' || number.length() != 11) {
 			System.out.println(number + " is an invalid social security number");
-		}else {
+			
+		} else {
 			System.out.println(number + " is a valid social security number");
 
 		}
-		
+
+		input.close();
 	}
 
 }
---
+++ Chapter04/src/main/Solution04_22.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.Scanner;
+
+public class Solution04_22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter string s1: ");
+		String s1 = input.next();
+		System.out.println("Enter string s2: ");
+		String s2 = input.next();
+
+		if (s1.contains(s2)) {
+			System.out.println(s2 + " is a substring of " + s1);
+		}else {
+			System.out.println(s2 + " is not a substring of " + s1);
+		}
+
+		input.close();
+	}
+
+}
