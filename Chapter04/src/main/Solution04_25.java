c3a13e1e9eeec904811941aaeed224d7ec34559f
samedsay <say16@itu.edu.tr>
2020-08-19 23:36:25 +0200

    updated

--- Chapter04/src/main/Solution04_25.java
+++ Chapter04/src/main/Solution04_25.java
@@ -10,18 +10,18 @@ public class Solution04_25 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		char first = (char)(int)((Math.random() * 26) + 65);
-		char second = (char)(int)((Math.random() * 26) + 65);
-		char third = (char)(int)((Math.random() * 26) + 65);
-		char fourth = (char)(int)((Math.random() * 9) + 48);
-		char fifth = (char)(int)((Math.random() * 9) + 48);
-		char sixth = (char)(int)((Math.random() * 9) + 48);
-		char seventh = (char)(int)((Math.random() * 9) + 48);
-		String numberPlate = Character.toString(first) + Character.toString(second) + Character.toString(third) +
-				Character.toString(fourth) + Character.toString(fifth) + Character.toString(sixth) + Character.toString(seventh);
+		char first = (char) (int) ((Math.random() * 26) + 65);
+		char second = (char) (int) ((Math.random() * 26) + 65);
+		char third = (char) (int) ((Math.random() * 26) + 65);
+		char fourth = (char) (int) ((Math.random() * 9) + 48);
+		char fifth = (char) (int) ((Math.random() * 9) + 48);
+		char sixth = (char) (int) ((Math.random() * 9) + 48);
+		char seventh = (char) (int) ((Math.random() * 9) + 48);
+		String numberPlate = Character.toString(first) + Character.toString(second) + Character.toString(third)
+				+ Character.toString(fourth) + Character.toString(fifth) + Character.toString(sixth)
+				+ Character.toString(seventh);
 
 		System.out.println("Random plate number is " + numberPlate);
-		
 
 	}
 
