1d986a57842465cf44a3153ea9e6539d0d16e706
samedsay <say16@itu.edu.tr>
2020-08-22 17:29:12 +0200

    updated

--- Chapter05/src/main/Solution05_10.java
+++ Chapter05/src/main/Solution05_10.java
@@ -3,20 +3,21 @@ package main;
 import java.util.Scanner;
 
 /*
- * (Find the highest score)
+ * (Find the two highest scores)
  * Write a program that prompts the user to enter the number of students and each student’s name and score,
- * and finally displays the name of the student with the highest score.
+ * and finally displays the student with the highest score and the student with the second-highest score.
  */
 
-public class Solution05_09 {
+public class Solution05_10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		Scanner input = new Scanner(System.in);
 		int grade;
 		int highestGrade = 0;
+		int secondHighestGrade = 0;
 		String highestGradeStudentName = "";
+		String secondHighestGradeStudentName = "";
 		do {
 			System.out.println("Enter student name: ");
 			String name = input.next();
@@ -27,10 +28,15 @@ public class Solution05_09 {
 				highestGrade = grade;
 				highestGradeStudentName = name;
 			}
+			if (grade > secondHighestGrade && grade < highestGrade) {
+				secondHighestGrade = grade;
+				secondHighestGradeStudentName = name;
+			}
 
 		} while (grade != -1);
 
 		System.out.println("Highest grade is " + highestGrade + " by " + highestGradeStudentName);
+		System.out.println("Second highest grade is " + secondHighestGrade + " by " + secondHighestGradeStudentName);
 
 		input.close();
 
