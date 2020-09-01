72e8eb882b62f3bf3d3e5c7b8438b904b43ab14d
samedsay <say16@itu.edu.tr>
2020-09-01 07:17:25 +0200

    updated

--- Chapter05/src/main/Solution05_25.java
+++ Chapter05/src/main/Solution05_25.java
@@ -5,38 +5,25 @@ public class Solution05_25 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double pi = 0;
-		for (int i = 0; i <= 10000; i++) {
+		for (int i = 1; i <= 10000; i++) {
 			pi += (Math.pow(-1, i + 1) / (2.0 * i - 1));
 		}
 		pi *= 4;
 		System.out.println("PI is " + pi + " for i = 10000");
 
 		pi = 0;
-		for (int i = 0; i <= 20000; i++) {
+		for (int i = 1; i <= 20000; i++) {
 			pi += (Math.pow(-1, i + 1) / (2.0 * i - 1));
 		}
 		pi *= 4;
 		System.out.println("PI is " + pi + " for i = 20000");
 
 		pi = 0;
-		for (int i = 0; i <= 100000; i++) {
+		for (int i = 1; i <= 100000; i++) {
 			pi += (Math.pow(-1, i + 1) / (2.0 * i - 1));
 		}
 		pi *= 4;
 		System.out.println("PI is " + pi + " for i = 100000");
-
-		// Compute PI value for i = 10000,
-		double sum = 0;
-		double value = 10000.0;
-		for (double d = 1; d <= (2 * value - 1); d += 2) {
-			sum += 1 / d;
-			d += 2;
-			sum -= 1 / d;
-		}
-		double pi2 = 4 * sum;
-
-		// Display result
-		System.out.println("PI value for i = 10000: " + pi2);
 
 	}
 
