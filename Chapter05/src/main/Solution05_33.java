5499cc49a240bcac258bfd51421be1f5765ac699
samedsay <say16@itu.edu.tr>
2020-09-16 01:25:13 +0200

    updated

---
+++ Chapter05/src/main/Solution05_33.java
@@ -0,0 +1,24 @@
+package main;
+
+/*
+ * (Perfect number)
+ * A positive integer is called a perfect number if it is equal to the sum of all of its positive divisors, excluding itself.
+ * For example, 6 is the first perfect number because 6 = 3 + 2 + 1.
+ * The next is 28 = 14 + 7 + 4 + 2 + 1.
+ * There are four perfect numbers less than 10,000.
+ * Write a program to find all these four numbers.
+ */
+
+public class Solution05_33 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 0; i < 10000; i++) {
+			
+			
+			
+		}
+
+	}
+}
