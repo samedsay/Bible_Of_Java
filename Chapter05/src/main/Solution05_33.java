70122b45efb24e977469e7a1121fc72806533d6c
samedsay <say16@itu.edu.tr>
2020-09-17 23:02:56 +0200

    updated

--- Chapter05/src/main/Solution05_33.java
+++ Chapter05/src/main/Solution05_33.java
@@ -14,9 +14,20 @@ public class Solution05_33 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int i = 0; i < 10000; i++) {
-			
-			
+		System.out.print("Perfect numbers: ");
+		int perfectNumber = 0;
+		for(int i = 1; i < 10000; i++) {
+			int sum = 0;
+			for(int j = 1; j < i; j++) {
+				if(i % j == 0) {
+					sum += j;
+				}
+				if(sum == i && i > perfectNumber) {
+					perfectNumber = i;
+					System.out.print(i + ", ");
+				}
+				
+			}
 			
 		}
 
