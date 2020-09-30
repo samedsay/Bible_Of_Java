39e33106d060da33134d2c228c7cd15debc6e44b
samedsay <say16@itu.edu.tr>
2020-09-30 21:47:54 +0200

    updated

---
+++ Chapter05/src/main/Solution05_37.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.util.Scanner;
+
+public class Solution05_37 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter a decimal number to see in binary format: ");
+		int decimalNumber = input.nextInt();
+		int power = 0;
+		int binaryNumber = 0;
+		System.out.print(decimalNumber);
+	
+		while (decimalNumber > 0) {
+
+			int remainder = decimalNumber % 2;
+			decimalNumber /= 2;
+			binaryNumber += remainder * Math.pow(10, power++);
+		}
+		
+		System.out.println(" (Decimal) = " + binaryNumber  + " (Binary)");
+
+	}
+
+}
