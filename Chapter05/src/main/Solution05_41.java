54f4867a2d211308ff4a7925564ce0ef909f77d8
samedsay <say16@itu.edu.tr>
2020-10-01 00:46:41 +0200

    updated

--- Chapter05/src/main/Solution05_41.java
+++ Chapter05/src/main/Solution05_41.java
@@ -1,5 +1,16 @@
 package main;
 
+/*
+ * (Occurrence of max numbers) Write a program that reads integers, finds the largest of them, and counts its occurrences.
+ * Assume that the input ends with number 0.
+ * Suppose that you entered 3 5 2 5 5 5 0; the program finds that the largest is 5 and the occurrence count for 5 is 4.
+ * (Hint: Maintain two variables, max and count.
+ * max stores the current max number, and count stores its occurrences. Initially, assign the first number to max and 1 to count.
+ * Compare each subsequent number with max.
+ * If the number is greater than max, assign it to max and reset count to 1.
+ * If the number is equal to max, increment count by 1.)
+ */
+
 import java.util.Scanner;
 
 public class Solution05_41 {
@@ -26,6 +37,8 @@ public class Solution05_41 {
 			System.out.println("The occurrence count of the largest number is " + countOfMax);
 		else
 			System.out.println("No entered numbers! Try Again!");
+
+		input.close();
 
 	}
 
---
+++ Chapter05/src/main/Solution05_42.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.util.Scanner;
+
+public class Solution05_42 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter comission rates: ");
+		System.out.println("Rate for 0 - 5000$");
+		double firstRate = input.nextDouble();
+		System.out.println("Rate for 5000$ - 10000$");
+		double secondRate = input.nextDouble();
+		System.out.println("Rate for > 10000$");
+		double thirdRate = input.nextDouble();
+
+		double amount = 0;
+		double interest = 0;
+
+		for (; interest < 30000; amount++) {
+			interest = 0;
+			if (amount >= 10000) {
+				interest += 5000 * firstRate;
+				interest += 5000 * secondRate;
+				interest += (amount - 10000) * thirdRate;
+			} else if (amount >= 5000) {
+				interest += 5000 * firstRate;
+				interest += (amount - 5000) * secondRate;
+			} else {
+				interest += amount * firstRate;
+			}
+
+		}
+
+		System.out.println("Minimum sales is " + amount);
+
+	}
+
+}
