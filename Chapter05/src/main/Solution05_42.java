c4c632f5e68383d34a6e404ab44d8f58d1ddbeff
samedsay <say16@itu.edu.tr>
2020-10-14 19:29:03 +0200

    updated

--- Chapter05/src/main/Solution05_42.java
+++ Chapter05/src/main/Solution05_42.java
@@ -1,5 +1,12 @@
 package main;
 
+/*
+ * (Financial application: find the sales amount)
+ * Rewrite Programming Exercise 5.39 as follows:
+ * ■ Use a for loop instead of a do-while loop.
+ * ■ Let the user enter COMMISSION_SOUGHT instead of fixing it as a constant.
+ */
+
 import java.util.Scanner;
 
 public class Solution05_42 {
--- Chapter05/src/main/Solution05_43.java
+++ Chapter05/src/main/Solution05_43.java
@@ -7,8 +7,6 @@ public class Solution05_43 {
 		
 		
 		
-		
-		
 
 	}
 
