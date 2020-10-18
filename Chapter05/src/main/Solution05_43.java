9aa08a96be4c24e94ae9a84faa469eef7e4b9408
samedsay <say16@itu.edu.tr>
2020-10-18 21:48:06 +0200

    updated

--- Chapter05/src/main/Solution05_43.java
+++ Chapter05/src/main/Solution05_43.java
@@ -4,9 +4,16 @@ public class Solution05_43 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		
+		int counter = 0;
+		for (int i = 1; i <= 7; i++) {
+			for (int j = i; j <= 7; j++) {
+				if (i != j) {
+					System.out.println(i + " " + j);
+					counter++;
+				}
+			}
+		}
+		System.out.println("The total number of all combinations is " + counter);
 
 	}
 
