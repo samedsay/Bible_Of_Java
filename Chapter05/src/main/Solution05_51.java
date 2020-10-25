ec15cb9038cc9d62b6a74f52d51b77e1b2b17220
samedsay <say16@itu.edu.tr>
2020-10-25 23:30:50 +0100

    updated

--- Chapter05/src/main/Solution05_51.java
+++ Chapter05/src/main/Solution05_51.java
@@ -1,11 +1,13 @@
 package main;
 
+import java.util.Scanner;
+
 public class Solution05_51 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		Scanner input = new Scanner(System.in);
 
 	}
 
