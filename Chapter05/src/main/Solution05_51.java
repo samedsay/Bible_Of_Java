a65d8626640d535c51ffea52b5e04515474f3621
samedsay <say16@itu.edu.tr>
2020-10-30 14:49:02 +0100

    updated

--- Chapter05/src/main/Solution05_51.java
+++ Chapter05/src/main/Solution05_51.java
@@ -6,8 +6,34 @@ public class Solution05_51 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter the first string: ");
+		String first = input.nextLine();
+		System.out.println("Enter the second string: ");
+		String second = input.nextLine();
+		int minLength;
+		String maxPrefix = "";
+		if (first.length() >= second.length())
+			minLength = second.length();
+		else
+			minLength = first.length();
+
+		for (int i = 0; i < minLength; i++) {
+
+			if (first.charAt(i) == second.charAt(i)) {
+				maxPrefix += first.charAt(i);
+			} else
+				break;
+		}
+
+		if (maxPrefix == "")
+			System.out.println(first + " and " + second + " have no common prefix");
+		else
+			System.out.println("The common prefix is " + maxPrefix);
+		
+		input.close();
 
 	}
 
