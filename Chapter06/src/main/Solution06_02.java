9809aba8e45452ced558586a61676d0befa9d8ba
samedsay <say16@itu.edu.tr>
2020-12-02 12:23:53 +0100

    Updated

--- Chapter06/src/main/Solution06_02.java
+++ Chapter06/src/main/Solution06_02.java
@@ -1,12 +1,29 @@
 package main;
 
+import java.util.Scanner;
+
 public class Solution06_02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
+		Scanner input = new Scanner(System.in);
 
+		System.out.println("Enter a number : ");
+		int number = input.nextInt();
+		System.out.println("Sum of digits of " + number + " = " + sumDigits(number));
+
+		
+		input.close();
+	}
+
+	public static int sumDigits(long n) {
+		int sum = 0;
+		while (n > 0) {
+			int remainder = (int) n % 10;
+			sum += remainder;
+			n /= 10;
+		}
+		return sum;
 	}
 
 }
