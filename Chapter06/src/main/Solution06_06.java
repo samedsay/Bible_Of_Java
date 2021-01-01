81ed2b2b415c55a0bb2ea5c00e5b8595cd331c24
samedsay <say16@itu.edu.tr>
2021-01-02 00:13:12 +0100

    Updated

--- Chapter06/src/main/Solution06_06.java
+++ Chapter06/src/main/Solution06_06.java
@@ -1,9 +1,32 @@
 package main;
 
+import java.util.Scanner;
+
 public class Solution06_06 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Pleasen enter integer value: ");
+		int number = input.nextInt();
+		displayPattern(number);
+
+	}
+
+	public static void displayPattern(int n) {
+
+		for (int i = 1; i <= n; i++) {
+
+			for (int k = 1; k <= n - i; k++) {
+				System.out.print("  ");
+			}
+
+			for (int j = i; j > 0; j--) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
 
 	}
 
