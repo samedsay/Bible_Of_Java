347ebf58cd0aace8fae32267525ba4e11e7e5fdd
samedsay <say16@itu.edu.tr>
2021-01-05 00:07:26 +0100

    Updated

--- Chapter06/src/main/Solution06_09.java
+++ Chapter06/src/main/Solution06_09.java
@@ -1,10 +1,38 @@
 package main;
 
+import java.util.Scanner;
+
 public class Solution06_09 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+
+		double meter = 20.0;
+		double foot = 1.0;
+
+		for (int i = 0; i < 10; i++) {
+
+			System.out.printf("%3.1f\t%3.3f\t|\t%3.1f\t%3.3f\n", foot, footToMeter(foot), meter, meterToFoot(meter));
+			meter += 5;
+			foot++;
+
+		}
+
+		input.close();
+
+	}
+
+	public static double footToMeter(double foot) {
+
+		return 0.305 * foot;
+
+	}
+
+	public static double meterToFoot(double meter) {
+
+		return 3.279 * meter;
 	}
 
 }
