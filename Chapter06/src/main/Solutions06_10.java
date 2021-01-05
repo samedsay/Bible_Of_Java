57376eb6bb1069f5d6c2b735245c24cec143a3d8
samedsay <say16@itu.edu.tr>
2021-01-05 19:02:25 +0100

    Updated

--- Chapter06/src/main/Solutions06_10.java
+++ Chapter06/src/main/Solutions06_10.java
@@ -1,10 +1,36 @@
 package main;
 
+import java.util.Scanner;
+
 public class Solutions06_10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter integer ( < 1000) :");
+		int number = input.nextInt();
+
+		if (isPrime(number)) {
+			System.out.println(number + " is prime");
+		} else {
+			System.out.println(number + " is not prime");
+		}
+
+		input.close();
+
+	}
+
+	public static boolean isPrime(int number) {
+	
+		
+		for (int divisor = 2; divisor < number; divisor++) {
+			if (number % divisor == 0) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 }
