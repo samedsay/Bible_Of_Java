ad937da0785d860a99c668b569caaab93c78ca47
samedsay <say16@itu.edu.tr>
2021-01-10 02:26:32 +0100

    Updated

--- Chapter06/src/main/Solution06_14.java
+++ Chapter06/src/main/Solution06_14.java
@@ -1,11 +1,27 @@
 package main;
 
+import java.util.Scanner;
+
 public class Solution06_14 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter i value: ");
+		int iMax = input.nextInt();
+		
+		for(int i = 1; i < iMax; i++) {
+			
+		}
+		
 
+	}
+	
+	public static double mFunction(int i) {
+		
+		
+		return 0;
 	}
 
 }
