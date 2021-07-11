f197634c1bd120c4e0dd59b63112c1e3dddcb2af
samedsay <say16@itu.edu.tr>
2021-07-11 19:37:15 +0200

    Updated

--- Chapter06/src/main/Solution06_14.java
+++ Chapter06/src/main/Solution06_14.java
@@ -6,29 +6,37 @@ public class Solution06_14 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter i value: ");
 		int iMax = input.nextInt();
 		
-		double result = 0;
-		
-		for(int i = 1; i < iMax; i+=100) {
-			
-			result += 4 * (Math.pow(-1, i + 1) / (2 * i -1));
-			
-			System.out.printf("%d\t\tm(%4f) \n", i, result);
-			
-			
+		System.out.println("_i_______________m(i)_");
+
+		for (int i = 1; i <= iMax; i++) {
+			double result = 0;
+
+			result = 4 * mFunction(i);
+
+			if (i % 100 == 1)
+
+				System.out.printf("%d\t\t%4.4f \n", i, result);
+
 		}
-		
 
 	}
-	
-	public static double mFunction(int i) {
-		
-		
-		return 0;
+
+	public static double mFunction(int iMax) {
+
+		double result = 0.0;
+
+		for (int i = 1; i <= iMax; i++) {
+
+			result += (Math.pow(-1, i + 1) / ((2 * i) - 1));
+
+		}
+
+		return result;
 	}
 
 }
